0e554cc16c0e2e55bb97ac1dae322193cb2c0144
manoutbar <manoutbar@alum.us.es>
2022-01-24 17:35:02 +0100

    applied theme style to app baseline

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from 'components/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container'; 
+import { styled } from '@mui/system';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import routes from 'routes';
@@ -12,24 +13,29 @@ import { useTranslation } from "react-i18next";
 
 import Theme from 'styles/theme';
 
+const StyledContainer = styled(Container)(({ theme }) => ({
+  backgroundColor: theme.palette.background.default,
+  marginTop: theme.spacing(3)
+}))
+
 function App() {
   const { t } = useTranslation();
   return (
     <React.Fragment>
-      <CssBaseline />
       <Router>
         <ThemeProvider theme={ Theme }>
+            <CssBaseline />
             <Toolbar>
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>{ t('commons:projectName') }</Typography>
             </Toolbar>
 
-            <Container maxWidth="xl">
+            <StyledContainer maxWidth="xl">
               <Switch>
                 { routes.map((route, idx) => (
                     <Route key={idx} {...route } />
                 ))}
               </Switch>
-            </Container>
+            </StyledContainer>
         </ThemeProvider>
       </Router>
     </React.Fragment>
