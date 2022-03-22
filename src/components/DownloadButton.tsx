6e1b3ea410ef70e822e74649f91ec306aad227e7
jose.lopez <jose.lopez@iwt2.org>
2022-03-22 15:00:35 +0100

    [hotfix]: fixed the json generator

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -1,29 +1,36 @@
 import React, { useContext } from 'react';
 import { Button, Grid, Theme } from "@mui/material";
 import { ThemeContext } from '@emotion/react';
-import { wizardSelector } from 'features/experiment/wizard/slice';
+import { wizardSelector} from 'features/experiment/wizard/slice';
 import { experimentsSelector } from 'features/experiment/slice';
-import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from "react-router-dom";
 import DownloadIcon from '@mui/icons-material/Download';
 import SendIcon from '@mui/icons-material/Send';
 import configuration from "infrastructure/util/configuration";
+import { CategoryDTO } from 'infrastructure/http/dto/wizard';
+import { useSelector } from 'react-redux';
 
 export interface DownloadButtonProps {
   filename: string,
   scenario_variability_mode: boolean,
 }
 
-const DownloadButton: React.FC<DownloadButtonProps> = ({ filename, scenario_variability_mode}) => {
+const DownloadButton: React.FC<DownloadButtonProps> = ({ filename, scenario_variability_mode }) => {
   const history = useHistory();
   const theme = useContext(ThemeContext) as Theme;
   const { t } = useTranslation();
   const { detail } = useSelector(experimentsSelector);
-  const { scenario_variability, seed } = useSelector(wizardSelector);
+  const { scenario_variability, seed, category_gui_components } = useSelector(wizardSelector);
+
+  function selectGUICatLabel(categories: CategoryDTO[]) {
+    let labels: string[] = categories.map(c => c.name)
+    return labels;
+  }
+
   let typeTMP = "text/json"
   let jsonTMP = seed;
-  if (scenario_variability_mode){
+  if (scenario_variability_mode) {
     jsonTMP = scenario_variability;
   }
 
@@ -40,33 +47,55 @@ const DownloadButton: React.FC<DownloadButtonProps> = ({ filename, scenario_vari
     a.dispatchEvent(clickEvt)
     a.remove()
   }
-  
+
+  function transformJson(jsonTMP:any){
+    if (!scenario_variability_mode) {
+      let tmpColumns, firstRowFile
+      let firstRowkey = Object.keys(jsonTMP)[0]
+      let secondRowkey = Object.keys(jsonTMP[firstRowkey])[0]
+      firstRowFile = Object.keys(jsonTMP[firstRowkey][secondRowkey])
+      let notList = ["Case", "Activity", "Screenshot", "Variant", ""]
+      tmpColumns = firstRowFile.filter(function (obj: any) { return notList.indexOf(obj) === -1; })
+      if (tmpColumns.indexOf("TextInput") > -1) {
+        tmpColumns[tmpColumns.indexOf("TextInput")] = "features.experiment.GUI_category.name.TextInput"
+      }
+      selectGUICatLabel(category_gui_components)
+      jsonTMP = {
+        "columnsNames": tmpColumns,
+        "GUIElements": selectGUICatLabel(category_gui_components),
+        "trace": jsonTMP
+      }
+    }
+    return jsonTMP
+  }
+
   function handleDownload(e: any) {
-    downloadFunction(jsonTMP, typeTMP, filename);
+   
+    downloadFunction(transformJson(jsonTMP), typeTMP, filename);
   }
 
   function handleDownloadAndNext(e: any) {
     if (window.confirm(t('features.experiment.assist.download_confirmation'))) {
-      downloadFunction(jsonTMP, typeTMP, filename);
-      history.push(configuration.PREFIX+'/experiment/'+detail?.id);
+      downloadFunction(transformJson(jsonTMP), typeTMP, filename);
+      history.push(configuration.PREFIX + '/experiment/' + detail?.id);
     }
   }
-    
-  
-  return  (
+
+
+  return (
     <Grid
-        container
-        direction="row-reverse"
-        spacing={2}>
-        <Grid xs={12} lg={4} item justifyContent="center" style={{ margin: theme.spacing(2) }}>
-          <Button type="submit" name="download" variant="contained" color="primary" style={{ margin: theme.spacing(2)}} endIcon={<DownloadIcon />} onClick={handleDownload}>
-            {t('features.experiment.assist.download')}
-          </Button>
-          <Button type="submit" name="generate" variant="contained" color="primary" endIcon={<SendIcon />} onClick={handleDownloadAndNext}>
-            {t('features.experiment.assist.download_next')}
-          </Button>
-        </Grid>
+      container
+      direction="row-reverse"
+      spacing={2}>
+      <Grid xs={12} lg={4} item justifyContent="center" style={{ margin: theme.spacing(2) }}>
+        <Button type="submit" name="download" variant="contained" color="primary" style={{ margin: theme.spacing(2) }} endIcon={<DownloadIcon />} onClick={handleDownload}>
+          {t('features.experiment.assist.download')}
+        </Button>
+        <Button type="submit" name="generate" variant="contained" color="primary" endIcon={<SendIcon />} onClick={handleDownloadAndNext}>
+          {t('features.experiment.assist.download_next')}
+        </Button>
       </Grid>
+    </Grid>
   );
 }
 
--- src/features/experiment/wizard/ActivitySelection.tsx
+++ src/features/experiment/wizard/ActivitySelection.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import { Button, Typography } from '@mui/material';
-import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { Link as RouterLink } from 'react-router-dom';
 import Table from '@mui/material/Table';
@@ -10,11 +10,13 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { wizardSelector } from 'features/experiment/wizard/slice';
 import { experimentsSelector } from 'features/experiment/slice';
 import BackButton from 'components/BackButton';
 import DownloadButton from 'components/DownloadButton';
 import configuration from "infrastructure/util/configuration";
+import { wizardSelector, guiComponentCategoryRepository, wizardSlice } from 'features/experiment/wizard/slice';
+import { CategoryDTO, CategoryResponse, } from 'infrastructure/http/dto/wizard';
+import { authSelector } from 'features/auth/slice';
 
 export interface ExperimentFormProperties {
   onSubmit: any
@@ -24,71 +26,87 @@ export interface ExperimentFormProperties {
 
 const ExperimentAssist: React.FC = () => {
   const { t } = useTranslation();
-  const { seed } = useSelector(wizardSelector);
+  const { seed, category_gui_components } = useSelector(wizardSelector);
   const { detail } = useSelector(experimentsSelector);
+  const [guiComponent, setGuiComponent] = useState(true);
+  const dispatch = useDispatch();
+  const { token } = useSelector(authSelector);
+
+  const getGUICat = async (token: string) => {
+    let categories: CategoryResponse = await guiComponentCategoryRepository.list(token);
+    dispatch(wizardSlice.actions.setGUIComponentCategories([...categories.results]));
+    setGuiComponent(false)
+  }
+
+  useEffect(() => {
+    if (guiComponent) {
+      getGUICat(token??"")
+    }
+  }, []);
 
   const variantActivities = (entry: any) => {
     let variant = entry[0];
     let acts = entry[1];
-    
-    return Object.keys(acts).map( act => (
+
+    return Object.keys(acts).map(act => (
       <TableRow
         key={`${variant}-${act}`}
         sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-        >
-            <TableCell align="center">
-              <Button
-                variant="contained"
-                component={RouterLink}
-                to={`${configuration.PREFIX}/column-variability/${variant}/${act}`}
-                >
-                {t("features.wizard.activitySelection.activity")} {act}
-              </Button>
-            </TableCell>
-            <TableCell>
-              {Object.keys(seed[variant][act]).some(column => seed[variant][act][column]['variate'] === 1) && (
-                <div>
-                    Configured
-                    {/* <DoneIcon />Variability configured */}
-                  </div>
-              )}
+      >
+        <TableCell align="center">
+          <Button
+            variant="contained"
+            component={RouterLink}
+            to={`${configuration.PREFIX}/column-variability/${variant}/${act}`}
+          >
+            {t("features.wizard.activitySelection.activity")} {act}
+          </Button>
+        </TableCell>
+        <TableCell>
+          {Object.keys(seed[variant][act]).some(column => seed[variant][act][column]['variate'] === 1) && (
+            <div>
+              Configured
+              {/* <DoneIcon />Variability configured */}
+            </div>
+          )}
 
-              {!Object.keys(seed[variant][act]).some(column => seed[variant][act][column]['variate'] === 1) && (
-                  <div>
-                    Not configured
-                    {/* <CloseIcon /> Variability not configured */}
-                  </div>
-              )}
-            </TableCell>
-          </TableRow>
-      ))};
+          {!Object.keys(seed[variant][act]).some(column => seed[variant][act][column]['variate'] === 1) && (
+            <div>
+              Not configured
+              {/* <CloseIcon /> Variability not configured */}
+            </div>
+          )}
+        </TableCell>
+      </TableRow>
+    ))
+  };
 
   return (
     <div>
       <Typography variant="h5">
-          <BackButton to={(detail!==null)?`${configuration.PREFIX}/experiment/${detail.id}`:`${configuration.PREFIX}/`} />
-          { t('features.experiment.create.title') }
+        <BackButton to={(detail !== null) ? `${configuration.PREFIX}/experiment/${detail.id}` : `${configuration.PREFIX}/`} />
+        {t('features.experiment.create.title')}
       </Typography>
       <Paper sx={{ width: '70%', overflow: 'hidden', margin: 'auto' }}>
-      <TableContainer sx={{ maxHeight: '100%' }}>
-        {
-          Object.entries(seed).map(entry => (
-            <Table sx={{ minWidth: 650 }} key={`${entry[0]}`} aria-label="variant activity selection">
-            <TableHead>
-            <TableRow key="headers">
-              <TableCell align="center">{`${t("features.wizard.activitySelection.variant")} ${entry[0]}`}</TableCell>
-              <TableCell>{t("features.wizard.activitySelection.variate")}</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {variantActivities(entry)}
-          </TableBody>
-        </Table>
-        ))}
-      </TableContainer>
-      <DownloadButton filename='case_variability_configuration' scenario_variability_mode={false} />
-    </Paper>
-  </div>
+        <TableContainer sx={{ maxHeight: '100%' }}>
+          {
+            Object.entries(seed).map(entry => (
+              <Table sx={{ minWidth: 650 }} key={`${entry[0]}`} aria-label="variant activity selection">
+                <TableHead>
+                  <TableRow key="headers">
+                    <TableCell align="center">{`${t("features.wizard.activitySelection.variant")} ${entry[0]}`}</TableCell>
+                    <TableCell>{t("features.wizard.activitySelection.variate")}</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {variantActivities(entry)}
+                </TableBody>
+              </Table>
+            ))}
+        </TableContainer>
+        <DownloadButton filename='case_variability_configuration' scenario_variability_mode={false} />
+      </Paper>
+    </div>
   );
 }
 
